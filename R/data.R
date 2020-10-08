8fbba5da51fec87314a359d0b0b1e09c08f6768e
Sheridan Grant <sheridanlgrant@gmail.com>
2020-10-08 00:51:52 -0700

    added 2014 data

---
+++ R/data.R
@@ -0,0 +1,82 @@
+#' Ballot Orders for Attorney General
+#'
+#' Ballot orders for 2014 Texas Republican Primary
+#' election, attorney general race.
+#'
+#' @format A data frame with 254 rows and 4 variables:
+#' \describe{
+#'   \item{County}{name of Texas county}
+#'   \item{Branch}{candidate's ballot position}
+#'   \item{Paxton}{candidate's ballot position}
+#'   \item{Smitherman}{candidate's ballot position}
+#' }
+#' @source \url{mailto:dgrant@shsu.edu}
+"attorneygeneral"
+
+#' Ballot Orders for Comptroller
+#'
+#' Ballot orders for 2014 Texas Republican Primary
+#' election, comptroller race.
+#'
+#' @format A data frame with 254 rows and 4 variables:
+#' \describe{
+#'   \item{County}{name of Texas county}
+#'   \item{Hegar}{candidate's ballot position}
+#'   \item{Hilderbran}{candidate's ballot position}
+#'   \item{Medina}{candidate's ballot position}
+#'   \item{Torres}{candidate's ballot position}
+#' }
+#' @source \url{mailto:dgrant@shsu.edu}
+"comptroller"
+
+#' Ballot Orders for Lieutenant Governor
+#'
+#' Ballot orders for 2014 Texas Republican Primary
+#' election, lieutenant governor race.
+#'
+#' @format A data frame with 254 rows and 4 variables:
+#' \describe{
+#'   \item{County}{name of Texas county}
+#'   \item{Dewhurst}{candidate's ballot position}
+#'   \item{Patrick}{candidate's ballot position}
+#'   \item{Patterson}{candidate's ballot position}
+#'   \item{Staples}{candidate's ballot position}
+#' }
+#' @source \url{mailto:dgrant@shsu.edu}
+"lieutenantgovernor"
+
+#' Ballot Orders for Railroad Commissioner
+#'
+#' Ballot orders for 2014 Texas Republican Primary
+#' election, railroad commissioner race.
+#'
+#' @format A data frame with 254 rows and 4 variables:
+#' \describe{
+#'   \item{County}{name of Texas county}
+#'   \item{Berger}{candidate's ballot position}
+#'   \item{Boyuls}{candidate's ballot position}
+#'   \item{Christian}{candidate's ballot position}
+#'   \item{Sitton}{candidate's ballot position}
+#' }
+#' @source \url{mailto:dgrant@shsu.edu}
+"railroadcommissioner"
+
+#' Ballot Orders for Senator
+#'
+#' Ballot orders for 2014 Texas Republican Primary
+#' election, senator race.
+#'
+#' @format A data frame with 254 rows and 4 variables:
+#' \describe{
+#'   \item{County}{name of Texas county}
+#'   \item{Cleaver}{candidate's ballot position}
+#'   \item{Cope}{candidate's ballot position}
+#'   \item{Cornyn}{candidate's ballot position}
+#'   \item{Mapp}{candidate's ballot position}
+#'   \item{Reasor}{candidate's ballot position}
+#'   \item{Stockman}{candidate's ballot position}
+#'   \item{Stovall}{candidate's ballot position}
+#'   \item{Vega}{candidate's ballot position}
+#' }
+#' @source \url{mailto:dgrant@shsu.edu}
+"senator"
